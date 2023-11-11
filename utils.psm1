9cf7cff5df2278d6761f0a501672c6a74e113bd3
Hellaeh <me@hellaeh.com>
2023-11-11 15:12:53 +0300

    Added pwsh utils

---
+++ utils.psm1
@@ -0,0 +1,18 @@
+function get-mod($arg) {
+	$mod = $arg;
+	
+	# i hate pwsh
+	.{
+		if (-not $mod) {
+			$mod = $(pwd).path;
+		} else {
+			$mod = $(resolve-path $mod).path;
+		}
+
+		if (-not (test-path "$mod\modinfo.json")) {
+			throw [System.IO.FileNotFoundException] """modinfo.json"" not found."
+		}
+	} | out-null;
+	
+	return $mod;
+}
