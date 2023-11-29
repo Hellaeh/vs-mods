0f25ffb4cc5e44c33fbc9910b84d88b9bcbf9274
Hellaeh <me@hellaeh.com>
2023-11-29 21:04:33 +0300

    Normalizer: Bump 0.2.0
    Add harmony patch

--- Normalizer/src/core.cs
+++ Normalizer/src/core.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Linq;
+
+using HarmonyLib;
 using Vintagestory.API.Common;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.Server;
 
-namespace BlockNormalizer;
+namespace HelNormalizer;
 
 public class Core : ModSystem
 {
-	private int ChiseledBlockId;
+	public static GridRecipe Recipe { get; private set; }
+
+	private int chiseledBlockId;
 	private ICoreServerAPI sApi;
+
+	private Harmony harmony;
+	private const string harmonyId = "helnormalizerharmony";
 
 	public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Server;
 
@@ -17,21 +24,62 @@ public class Core : ModSystem
 	{
 		base.StartServerSide(api);
 
+		RegisterRecipe(api);
+
+		harmony = new Harmony(harmonyId);
+		harmony.PatchAll();
+
+		api.Event.MatchesGridRecipe += OnGridRecipeMatch;
+
+		sApi = api;
+	}
+
+	private void RegisterRecipe(ICoreServerAPI api)
+	{
 		const string CB = "chiseledblock";
+		const string PATTERN = "S";
+
 		var block = Array.Find(api.World.SearchBlocks(new(CB)), block => block.Code.GetName() == CB);
 
 		if (block == null)
 			return;
 
-		api.Event.MatchesGridRecipe += OnGridRecipeMatch;
+		var ingr = new CraftingRecipeIngredient()
+		{
+			Code = block.Code,
+			ResolvedItemstack = new(block)
+		};
 
-		ChiseledBlockId = block.Id;
-		sApi = api;
+		var resolved = ingr.CloneTo<GridRecipeIngredient>();
+		resolved.PatternCode = PATTERN;
+
+		Recipe = new GridRecipe()
+		{
+			Shapeless = true,
+			IngredientPattern = PATTERN,
+			Width = 1,
+			Height = 1,
+			Ingredients = new()
+			{
+				[PATTERN] = ingr
+			},
+			// Placeholder output will be replaced in `MatchedGridRecipe` event handler
+			Output = ingr,
+			Name = block.Code,
+			resolvedIngredients = new GridRecipeIngredient[1] { resolved }
+		};
+
+		api.RegisterCraftingRecipe(Recipe);
+		chiseledBlockId = block.Id;
 	}
 
 	private bool OnGridRecipeMatch(IPlayer player, GridRecipe recipe, ItemSlot[] inputSlots, int gridWidth)
 	{
-		var slotCount = 0;
+		// `MatchesGridRecipe` will fire for every single recipe in game, so we filter em
+		if (Recipe != recipe)
+			return true;
+
+		var count = 0;
 		ItemSlot match = null;
 
 		foreach (var slot in inputSlots)
@@ -39,51 +87,36 @@ public class Core : ModSystem
 			if (slot.Empty)
 				continue;
 
-			++slotCount;
+			++count;
 
-			if (slot.Itemstack.Id == ChiseledBlockId)
+			if (slot.Itemstack.Id == chiseledBlockId)
 				match = slot;
 		}
 
-		if (match == null || slotCount > 1)
+		if (match == null || count > 1)
 			return true;
 
 		var materials = (match.Itemstack.Attributes["materials"] as IntArrayAttribute)?.value;
 
-		if (materials == null)
-			return true;
-
-		var outputCount = match.Itemstack.StackSize;
-
 		// First id is initial block, which could be a variant `ew`, `ud` etc, that should not be obtained in survival
-		var first = materials[0];
-		var block = sApi.World.GetBlock(first);
-		foreach (var drop in block.Drops)
+		var initialBlock = sApi.World.GetBlock(materials[0]);
+		foreach (var drop in initialBlock.GetDrops(sApi.World, null, player).Take(1))
+			initialBlock = drop.Block ?? initialBlock;
+
+		recipe.Output = new()
 		{
-			var stack =
-				drop.ResolvedItemstack.Block == null
-					? new(block)
-					: drop.ResolvedItemstack;
+			Code = initialBlock.Code,
+			ResolvedItemstack = new(initialBlock),
+		};
 
-			stack.StackSize = outputCount;
+		return true;
+	}
 
-			if (!player.InventoryManager.TryGiveItemstack(stack))
-				sApi.World.SpawnItemEntity(stack, player.Entity.Pos.XYZ);
-
-			break;
-		}
-
-		foreach (var id in materials.Skip(1))
-		{
-			var stack = new ItemStack(sApi.World.GetBlock(id), outputCount);
-
-			if (!player.InventoryManager.TryGiveItemstack(stack))
-				sApi.World.SpawnItemEntity(stack, player.Entity.Pos.XYZ);
-		}
-
-		match.TakeOutWhole();
-
-		return false;
+	public override void Dispose()
+	{
+		harmony.UnpatchAll(harmonyId);
+		Recipe = null;
+		base.Dispose();
 	}
 }
 
---
+++ Normalizer/src/patch.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+
+using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+
+namespace HelNormalizer;
+
+[HarmonyPatch]
+public class Patch
+{
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(CollectibleObject), nameof(CollectibleObject.OnConsumedByCrafting))]
+#pragma warning disable
+	static bool OnConsumedByCraftingPostfix(
+		CollectibleObject __instance,
+		ItemSlot[] allInputSlots,
+		ItemSlot stackInSlot,
+		GridRecipe gridRecipe,
+		CraftingRecipeIngredient fromIngredient,
+		IPlayer byPlayer,
+		int quantity
+	)
+#pragma warning restore
+	{
+		if (Core.Recipe != gridRecipe)
+			return true;
+
+		var materials = (stackInSlot.Itemstack.Attributes["materials"] as IntArrayAttribute).value;
+
+		// Skip initial block, we already handled it
+		foreach (var id in materials.Skip(1))
+		{
+			var block = byPlayer.Entity.World.GetBlock(id);
+			var stack = new ItemStack(block, quantity);
+
+			if (!byPlayer.InventoryManager.TryGiveItemstack(stack, true))
+				byPlayer.Entity.World.SpawnItemEntity(stack, byPlayer.Entity.Pos.XYZ);
+		}
+
+		return true;
+	}
+}
