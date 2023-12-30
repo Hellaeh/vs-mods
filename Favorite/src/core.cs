f1b3b11ddb1b704852234bcf5b899ff89fb1170b
Hellaeh <me@hellaeh.com>
2023-12-30 19:35:50 +0300

    Favorite: Bump 0.2.0
    Track items from crafting grid to inventory on inventory close
    
    Fix item tracking on mouse click
    Fix drop items not working in some circumstances
    
    Rename "UpdateFavorite" to "Update"
    Mark "UpdateFavorite" as deprecated

--- Favorite/src/core.cs
+++ Favorite/src/core.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,10 +35,10 @@ public class Core : ModSystem
 
 	public ICoreClientAPI Api { get; private set; }
 
-	// FIXME: Remove. We need this later as a workaround to avoid crashing a client 
+	// FIXME: Remove. We need this later as a workaround to avoid crashing a client
 	// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
 	private GuiDialog backpackGui;
-	// FIXME: Remove. We need this later as a workaround to avoid crashing a client 
+	// FIXME: Remove. We need this later as a workaround to avoid crashing a client
 	// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
 	private GuiDialog hotbarGui;
 
@@ -45,6 +46,14 @@ public class Core : ModSystem
 
 	public string Color => config.FavoriteColor;
 
+	// FIXME: Remove. We need this later as a workaround to avoid crashing a client
+	// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
+	internal bool backpackGuiVisible;
+	internal bool shouldUpdate;
+
+	/// <summary>
+	/// A collection of slot indices by inventory
+	/// </summary>
 	public FavoriteSlots FavoriteSlots { get; private set; }
 
 	public bool MouseFavorite
@@ -85,10 +94,12 @@ public class Core : ModSystem
 
 	private void OnHotkeyChanged() => hotkeyCode = Api.Input.GetHotKeyByCode(hotkey).CurrentMapping.KeyCode;
 
-	// NOTE: Should throttle? 
-	private void OnBackpackSlotModified(int _) => UpdateFavorite(Backpack);
-	private void OnCraftingGridSlotModified(int _) => UpdateFavorite(CraftingGrid);
-	private void OnHotbarSlotModified(int _) => UpdateFavorite(Hotbar);
+	private void OnBackpackSlotModified(int _) => Update(Backpack);
+	private void OnCraftingGridSlotModified(int _) => Update(CraftingGrid);
+	private void OnHotbarSlotModified(int _) => UpdateHotbar();
+
+	private void OnBackpackOpened() { backpackGuiVisible = true; Update(Backpack); }
+	private void OnBackpackClosed() => backpackGuiVisible = false;
 
 	private void OnPlayerJoin(IClientPlayer player)
 	{
@@ -110,27 +121,32 @@ public class Core : ModSystem
 		foreach ((var inv, var favSlots) in FavoriteSlots)
 			favSlots.RemoveWhere(slotId => inv[slotId] == null || inv[slotId].Empty);
 
-		// FIXME: Remove. We need this later as a workaround to avoid crashing a client 
-		// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
-		Backpack.SlotModified += OnBackpackSlotModified;
-		CraftingGrid.SlotModified += OnCraftingGridSlotModified;
-		Hotbar.SlotModified += OnHotbarSlotModified;
-
-		UpdateFavorite();
+		Update();
 		HijackDropItemHotkeyHandler();
 
 		Api.Event.MouseDown += OnMouseDown;
 		Api.Event.MouseUp += OnMouseUp;
+
+		Backpack.SlotModified += OnBackpackSlotModified;
+		CraftingGrid.SlotModified += OnCraftingGridSlotModified;
+		Hotbar.SlotModified += OnHotbarSlotModified;
+
+		backpackGui.OnOpened += OnBackpackOpened;
+		backpackGui.OnClosed += OnBackpackClosed;
 	}
+
 
 	private void OnPlayerLeave()
 	{
+		Api.StoreModConfig(config, ClientConfigFile);
+		Api.StoreModConfig(new FavoriteSlotsConfig() { SlotsByInventory = FavoriteSlots.ToDictionary() }, FavoriteSlotsFile);
+
 		Backpack.SlotModified -= OnBackpackSlotModified;
 		CraftingGrid.SlotModified -= OnCraftingGridSlotModified;
 		Hotbar.SlotModified -= OnHotbarSlotModified;
 
-		Api.StoreModConfig(config, ClientConfigFile);
-		Api.StoreModConfig(new FavoriteSlotsConfig() { SlotsByInventory = FavoriteSlots.ToDictionary() }, FavoriteSlotsFile);
+		backpackGui.OnOpened -= OnBackpackOpened;
+		backpackGui.OnClosed -= OnBackpackClosed;
 	}
 
 	private void OnMouseDown(MouseEvent e)
@@ -140,15 +156,7 @@ public class Core : ModSystem
 
 		var slot = Api.World.Player.InventoryManager.CurrentHoveredSlot;
 
-		if (slot == null)
-		{
-			if (MouseFavorite)
-				e.Handled = true;
-
-			return;
-		}
-
-		if (slot.Empty)
+		if (slot == null || slot.Empty)
 			return;
 
 		if (!Api.Input.KeyboardKeyStateRaw[hotkeyCode])
@@ -170,14 +178,16 @@ public class Core : ModSystem
 
 		e.Handled = true;
 
-		UpdateFavorite(slot.Inventory);
+		Update(slot.Inventory);
 	}
 
 	private void OnMouseUp(MouseEvent e)
 	{
-		// Api.Logger.Warning($"UP - {Core.}");
-		if (Mouse.Empty)
+		if (Mouse.Empty && MouseFavorite)
 			MouseFavorite = false;
+
+		if (shouldUpdate)
+			Update();
 	}
 
 	private void HijackDropItemHotkeyHandler()
@@ -192,59 +202,109 @@ public class Core : ModSystem
 			{
 				var hoverSlot = Api.World.Player.InventoryManager.CurrentHoveredSlot;
 
-				return (hoverSlot == null || !hoverSlot.IsFavorite())
-					&& !FavoriteSlots[Hotbar].Contains(Api.World.Player.InventoryManager.ActiveHotbarSlotNumber)
-					&& originalHandler(key);
+				return (
+					hoverSlot == null
+					|| !hoverSlot.IsFavorite()
+					|| !FavoriteSlots[Hotbar].Contains(Api.World.Player.InventoryManager.ActiveHotbarSlotNumber)
+				)
+				&& originalHandler(key);
 			}
 
 			Api.Input.SetHotKeyHandler(hotkey, newHandler);
 		}
 	}
 
-	public void UpdateFavorite(IInventory forInv = null)
+	// TODO: Remove in release
+	[Obsolete("`UpdateFavorite` is renamed to `Update` and will be removed in 1.0.0")]
+	public void UpdateFavorite(IInventory inv = null) => Update(inv);
+
+	/// <summary>
+	/// Will update favorite slots and VFX
+	/// </summary>
+	public void Update(IInventory forInv = null)
 	{
-		foreach ((var inv, var favSlots) in FavoriteSlots.Where(kv => kv.Key != Mouse && (forInv == null || kv.Key == forInv)))
+		if (backpackGuiVisible)
 		{
-			// FIXME: Remove once you can dynamically color slot without a crash
-			// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
-			var shouldRecompose = true;
-			// Reset colors
-			foreach (var slot in inv)
-				// To ensure others mods keep their colors
-				if (slot.HexBackgroundColor == Color)
+			IInventory[] mainInvs = [Backpack, CraftingGrid];
+
+			foreach (var inv in mainInvs.Where(inv => forInv == null || forInv == inv))
+			{
+				var favSlots = FavoriteSlots[inv];
+
+				// FIXME: Remove once you can dynamically color slot without a crash
+				// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
+				var shouldRecompose = true;
+				// Reset colors
+				foreach (var slot in inv)
+					// To ensure others mods keep their colors
+					if (slot.HexBackgroundColor == Color)
+					{
+						slot.HexBackgroundColor = null;
+						shouldRecompose = false;
+					}
+
+				favSlots.RemoveWhere(slotId => inv[slotId] == null || inv[slotId].Empty);
+
+				// FIXME: Remove once you can dynamically color slot without a crash
+				// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
+				var didColor = false;
+				foreach (var id in favSlots)
 				{
-					slot.HexBackgroundColor = null;
-					shouldRecompose = false;
+					inv[id].HexBackgroundColor = Color;
+					didColor = true;
 				}
 
-			favSlots.RemoveWhere(slotId => inv[slotId] == null || inv[slotId].Empty);
-
-			// FIXME: Remove once you can dynamically color slot without a crash
-			// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
-			var didColor = false;
-			foreach (var id in favSlots)
-			{
-				inv[id].HexBackgroundColor = Color;
-				didColor = true;
+				// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
+				if (shouldRecompose && didColor)
+				{
+					// NOTE: No null checks cuz we crash anyway at this point or shortly after
+					backpackGui.Composers.First().Value.Dispose();
+					backpackGui.Composers.First().Value.ReCompose();
+				}
 			}
+		}
 
-			// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
-			if (inv == Hotbar || shouldRecompose && didColor)
+		if (forInv == null || forInv == Hotbar)
+			UpdateHotbar();
+	}
+
+	// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
+	// Hotbar is a special beast each slot can crash a game if not recomposed
+	private void UpdateHotbar()
+	{
+		var shouldRecompose = false;
+
+		for (int slotId = 0; slotId <= 10; ++slotId)
+		{
+			var slot = Hotbar[slotId];
+			var wasColored = slot.HexBackgroundColor == Color;
+
+			// Reset color
+			if (wasColored)
+				slot.HexBackgroundColor = null;
+
+			if (slot.Empty)
+				FavoriteSlots[Hotbar]?.Remove(slotId);
+
+			if (slot.IsFavorite(slotId))
 			{
-				// NOTE: No null checks cuz we crash anyway at this point
-				var comp = (inv == Hotbar ? hotbarGui : backpackGui).Composers.First().Value;
-				comp.Dispose();
-				comp.ReCompose();
+				slot.HexBackgroundColor = Color;
+
+				if (!wasColored)
+					shouldRecompose = true;
 			}
+		}
+
+		// ISSUE: https://github.com/anegostudios/VintageStory-Issues/issues/3305
+		if (shouldRecompose)
+		{
+			hotbarGui.Composers.First().Value.Dispose();
+			hotbarGui.Composers.First().Value.ReCompose();
 		}
 	}
 
 	public override void Dispose()
 	{
-		Backpack.SlotModified -= OnBackpackSlotModified;
-		CraftingGrid.SlotModified -= OnCraftingGridSlotModified;
-		Hotbar.SlotModified -= OnHotbarSlotModified;
-
 		Api.Event.PlayerJoin -= OnPlayerJoin;
 		Api.Event.LeaveWorld -= OnPlayerLeave;
 
--- Favorite/src/patch.cs
+++ Favorite/src/patch.cs
@@ -1,6 +1,7 @@
 using HarmonyLib;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.Client.NoObf;
 using Vintagestory.Common;
 
 namespace HelFavorite;
@@ -28,6 +29,90 @@ public class GuiElementItemSlotGridBasePatch
 		var inv = Traverse.Create(__instance).Field("inventory").GetValue<IInventory>();
 
 		Core.Instance.FavoriteSlots[inv]?.Add(intoSlotId);
+
+		Core.Instance.shouldUpdate = true;
+	}
+}
+
+[HarmonyPatch]
+public class GuiDialogInventoryPatch
+{
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(GuiDialogInventory), nameof(GuiDialogInventory.OnGuiClosed))]
+	// Move items from crafting grid back to inventory while keep track of favorite items
+	static bool OnGuiClosedPrefix()
+	{
+		if (Core.Instance?.CraftingGrid == null)
+			return true;
+
+		var player = Core.Instance.Api.World.Player;
+
+		if (player.WorldData.CurrentGameMode != EnumGameMode.Survival)
+			return true;
+
+		for (int slotId = 0; slotId < Core.Instance.CraftingGrid.Count; ++slotId)
+		{
+			var slot = Core.Instance.CraftingGrid[slotId];
+
+			if (slot.Empty || !slot.IsFavorite(slotId))
+				continue;
+
+			var op = new ItemStackMoveOperation(Core.Instance.Api.World, EnumMouseButton.Button1, 0, EnumMergePriority.AutoMerge, slot.StackSize)
+			{
+				ActingPlayer = player
+			};
+
+			object[] packets = player.InventoryManager.TryTransferAway(slot, ref op, true, false);
+
+			if (packets == null)
+			{
+				int backpackSlotId = Core.BagsOffset;
+				var shouldRetry = false;
+
+				for (; backpackSlotId < Core.Instance.Backpack.Count; ++backpackSlotId)
+				{
+					var backpackSlot = Core.Instance.Backpack[backpackSlotId];
+
+					if (backpackSlot.IsFavorite(backpackSlotId))
+						continue;
+
+					player.InventoryManager.DropItem(backpackSlot, true);
+
+					shouldRetry = true;
+
+					break;
+				}
+
+				if (shouldRetry)
+					packets = player.InventoryManager.TryTransferAway(slot, ref op, true, false);
+			}
+
+			var flags = System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance;
+
+			for (int i = 0; packets != null && i < packets.Length; ++i)
+			{
+				var packet = (Packet_Client)packets[i];
+
+				Core.Instance.Api.Network.SendPacketClient(packet);
+
+				if (
+					typeof(Packet_Client)
+						.GetField("MoveItemstack", flags)
+						.GetValue(packet)
+						is not Packet_MoveItemstack mvPacket
+				)
+					continue;
+
+				var targetInvId = (string)mvPacket.GetType().GetField("TargetInventoryId", flags).GetValue(mvPacket);
+				var targetSlotId = (int)mvPacket.GetType().GetField("TargetSlot", flags).GetValue(mvPacket);
+				var targetInv = player.InventoryManager.GetInventory(targetInvId);
+
+				Core.Instance.FavoriteSlots[targetInv]?.Add(targetSlotId);
+				Core.Instance.shouldUpdate = true;
+			}
+		}
+
+		return true;
 	}
 }
 
@@ -36,47 +121,61 @@ public class InventoryBasePatch
 {
 	[HarmonyPrefix]
 	[HarmonyPatch(typeof(InventoryBase), nameof(InventoryBase.ActivateSlot))]
-	// 1. Prevent `shift + click` on favorite slot
-	// 2. Track favorite item across player inventory
 	static bool ActivateSlotPrefix(InventoryBase __instance, int slotId, ItemSlot sourceSlot, ref ItemStackMoveOperation op)
 	{
 		if (op.MouseButton is not (EnumMouseButton.Left or EnumMouseButton.Right))
 			return true;
 
 		var inv = __instance;
-		var mouseSlot = sourceSlot;
 
 		if (inv.Api.Side == EnumAppSide.Server)
 			return true;
 
-		var fav = Core.Instance;
-		var favSlots = fav.FavoriteSlots[inv];
+		var mouseSlot = sourceSlot;
+		var targetSlot = inv[slotId];
 
-		if (!fav.MouseFavorite && (favSlots == null || !favSlots.Contains(slotId)))
+		var mouseEmpty = mouseSlot.Empty;
+		var targetEmpty = targetSlot.Empty;
+
+		if (mouseEmpty && targetEmpty)
+			return false;
+
+		var mouseFavorite = Core.Instance.MouseFavorite;
+		var targetFavorite = targetSlot.IsFavorite(slotId);
+
+		if (mouseFavorite == targetFavorite)
 			return true;
 
 		// Allow only direct merge for favorite slots
 		if (op.CurrentPriority != EnumMergePriority.DirectMerge)
 			return false;
 
-		sourceSlot = inv[slotId];
+		var matchItemstack = !targetEmpty && targetSlot.Itemstack.Satisfies(mouseSlot.Itemstack);
 
-		// FIXME: Improve and simplify logic
-		if (mouseSlot.Empty && !sourceSlot.Empty)
-			return fav.MouseFavorite = true;
-
-		if (fav.MouseFavorite)
+		if (mouseFavorite) // mouse is favorite, target is not
 		{
-			if (sourceSlot.IsFavorite(slotId))
+			if (mouseEmpty)
 				return true;
 
-			if (!sourceSlot.Empty && sourceSlot.Itemstack != mouseSlot.Itemstack)
-				fav.MouseFavorite = false;
+			// left or right click will swap mouse and target
+			if (!targetEmpty && !matchItemstack)
+				Core.Instance.MouseFavorite = false;
 
-			favSlots?.Add(slotId);
+			Core.Instance.FavoriteSlots[inv]?.Add(slotId);
+		}
+		else // target is favorite, mouse is not
+		{
+			if (targetEmpty)
+				return true;
+
+			if (mouseEmpty || !matchItemstack)
+				Core.Instance.MouseFavorite = true;
+
+			if (!mouseEmpty && !matchItemstack)
+				Core.Instance.FavoriteSlots[inv]?.Remove(slotId);
 		}
 
-		return true;
+		return Core.Instance.shouldUpdate = true;
 	}
 
 	[HarmonyPrefix]
@@ -101,7 +200,7 @@ public class InventoryBasePatch
 
 				Core.Instance.FavoriteSlots[destInv].Add(destInv.GetSlotId(notFavSlot));
 
-				return true;
+				return Core.Instance.shouldUpdate = true;
 			}
 
 			return false;
